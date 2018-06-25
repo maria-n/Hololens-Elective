aa8b82474b1086d4b8eaba75c321215ff05389b6
ChrisBool <christianefilb@gmail.com>
2018-06-25 12:03:25 +0200

    Initial Project Commit

---
+++ MuseumUnderwaterDemo/Assets/Scripts/OctiSpeechInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class OctiSpeechInteraction : MonoBehaviour, ISpeechHandler
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        switch (eventData.RecognizedText)
+        {
+            case "Hello":
+                Debug.Log("Recognized Hello");
+                gameObject.GetComponent<AudioSource>().Play();
+                break;
+
+            case "Test":
+                Debug.Log("Testing");
+                //gameObject.GetComponent<AudioSource>().Play();
+                break;
+
+
+        }
+    }
+}
---
+++ MuseumUnderwaterDemo/Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class SpeechManager : MonoBehaviour, ISpeechHandler
+{
+
+    public GameObject[] placementObjects;
+    public string[] placementObjectStrings;
+
+    public static SpeechManager Instance { set; get; }
+    public bool developerMode = false;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (developerMode)
+        {
+
+        }
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        switch (eventData.RecognizedText)
+        {
+            case "Developer":
+                Debug.Log("Dev Mode toggled");
+                developerMode = !developerMode;
+                break;
+        }
+
+        if (developerMode)
+        {
+            for (int i = 0; i < placementObjects.Length; i++)
+            {
+                if (eventData.RecognizedText == placementObjectStrings[i])
+                {
+                    //set placementObjects as placement thingy
+                }
+            }
+
+        }
+    }
+
+}
